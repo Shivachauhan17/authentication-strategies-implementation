0a74ff30c23ee1b51ae060c16c50569cd50405e1
Shivachauhan17 <shivanbd2019@gmail.com>
2023-11-15 17:01:36 +0530

    successfully implemented Oauth

--- jwt/config/passport.js
+++ jwt/config/passport.js
@@ -1,13 +1,41 @@
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
 const fs = require('fs');
 const path = require('path');
-const User = require('mongoose').model('User');
+const User = require('../models/user');
 
 
 const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
 const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
 
 // TODO
-const options = {};
+const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: PUB_KEY,
+    algorithms: ['RS256']
+  };
 
 // TODO
-module.exports = (passport) => {}
+module.exports = (passport) => {
+
+        passport.use(new JwtStrategy(options,(jwt_payload,done)=>{
+
+            console.log(jwt_payload)
+
+            User.findOne({_id:jwt_payload.sub})
+                .then(user=>{
+                    if(user)
+                        return done(null,user);
+                    else
+                        return done(null,false);
+                })
+                .catch(err=>{
+                    return done(null,false);
+                })
+
+        }
+        )
+        )
+
+}
