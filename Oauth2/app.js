bbb42eb8e3111ba6f69d012d60ef847bc1f0cc97
Shivachauhan17 <shivanbd2019@gmail.com>
2023-11-16 20:03:40 +0530

    started implementing open auth

--- Oauth2/app.js
+++ Oauth2/app.js
@@ -1,42 +1,24 @@
 require('dotenv').config()
 
-const createError=require('http-errors')
 const express=require('express')
-const path=require('path')
-const cookieParser=require('cookie-parser')
-const session=require('express-session')
-const csrf=require('csurf')
+const cors=require('cors')
 const passport=require('passport')
 const logger=require('morgan')
-const mongoStore=require('connect-mongo')
-const connectDB=require('./db').connectDB
-app.use(connectDB())
+const connectDB=require('./db')
+const router=require('./routes/auth')
+connectDB()
 
 const app=express()
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false, // don't save session if unmodified
-    saveUninitialized: false, // don't create session until something stored
-    store:mongoStore.create({
-                mongoUrl: process.env.DB_STRING,
-                collection: 'sessions'
-              })  
-  }));
 
-  app.use(csrf());
-  app.use(passport.authenticate('session'));
-  app.use(function(req, res, next) {
-    res.locals.csrfToken = req.csrfToken();
-    next();
-  });
 
-  app.use(function(req, res, next) {
-    next(createError(404));
-  });
-  module.exports = app;
+app.use(router);
+
+app.listen(8000,()=>{
+  console.log('server is running you better catch it')
+})
---
+++ Oauth2/config/passport.js
@@ -0,0 +1,17 @@
+
+const FacebookStrategy=require('passport-facebook').Strategy
+
+module.exports=(passport)=>{
+    passport.use(new FacebookStrategy({
+        clientID: FACEBOOK_APP_ID,
+        clientSecret: FACEBOOK_APP_SECRET,
+        callbackURL: "http://localhost:3000/auth/facebook/callback",
+        profileFields: ['id', 'displayName', 'photos', 'email']
+      },
+      function(accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+          return cb(err, user);
+        });
+      }
+    ));
+}
